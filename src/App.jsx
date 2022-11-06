af3369de2a0cbf07f9286ca2a1a3bfbbed4de51e
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2022-11-06 20:07:15 +0700

    build: implement router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { io } from 'socket.io-client';
+
+import Chat from './pages/Chat';
+import Home from './pages/Home';
 
 const socket = io('http://localhost:5000');
 socket.on('connect', () => {
   console.log('🟢: online');
 });
 
-function App() {
+const App = () => {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-center font-bold">Demo - Chat App</h1>
-    </div>
+    <BrowserRouter>
+      <div className="container mx-auto p-4">
+        <Routes>
+          <Route path="/" element={<Home socket={socket} />} />
+          <Route path="/chat" element={<Chat socket={socket} />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
